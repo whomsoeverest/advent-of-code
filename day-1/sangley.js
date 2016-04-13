c4a728c5b37d3f92aac60110a9f9c6cfae8ed649
sangley <seanangley@gmail.com>
2016-04-13 18:41:47 -0400

    day 1 puzzle 2

--- day-1/sangley.js
+++ day-1/sangley.js
@@ -4,3 +4,17 @@ var floorCalc = function(input) {
 }
 
 /* day 1 puzzle 2 */
+var basementCalc = function(input) {
+    for(var i=0,len = input.length,currFloor=0;i<len;i++) {
+        var dir = input.charAt(i);
+        if(dir == '(') {
+            currFloor += 1;
+        }
+        else if(dir == ')') {
+            currFloor -= 1;
+        }
+        if(currFloor < 0) {
+            return ++i;
+        }
+    }
+}
