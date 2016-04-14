e7e75c6f360b0c779906c89f8f9e0b1275dba46e
sangley <seanangley@gmail.com>
2016-04-13 22:36:49 -0400

    day 3 pt 2

--- day-3/sangley.js
+++ day-3/sangley.js
@@ -57,3 +57,67 @@ var luckyHouses = function(input) {
         ,housesVisited: totalHouses
     };
 }
+
+/* day 3 part 2 */
+var roboDelivery = function(input) {
+    var deliverers = {santa:{x:0,y:0},roboSanta:{x:0,y:0}}
+        ,totalHouses = 0
+        ,luckyHouses = [0]
+        ,houseMap = []
+        ,currChar;
+    
+    var goToHouse = function(x,y) {
+        if(houseMap[x] === undefined) {
+            houseMap[x] = [];
+        }
+        if(houseMap[x][y] === undefined) {
+            totalHouses++;
+            houseMap[x][y] = 0;
+        }
+        currentPresents = ++houseMap[x][y];
+        
+        if(luckyHouses[currentPresents] === undefined) {
+            luckyHouses[currentPresents] = 0;
+        }
+        luckyHouses[currentPresents]++;
+        
+        if(currentPresents - 1 > 0) {
+            luckyHouses[currentPresents - 1]--;
+        }
+    }
+    
+    for(var i = 0, base = Object.keys(deliverers).length; i<base; i++) {
+        goToHouse(0,0);
+    }
+    
+    var currDriverPos = -1
+        ,currDriverName
+        ,totalHouses = base ? 1 : 0;    
+    for(var i = 0, len = input.length; i < len; i++) {
+        currChar = input.charAt(i);
+        if(++currDriverPos == base) {
+            currDriverPos = 0;
+        } 
+        currDriverName = Object.keys(deliverers)[currDriverPos];
+        
+        if(currChar == '^') {
+            deliverers[currDriverName].x++;
+        }
+        else if(currChar == 'v') {
+            deliverers[currDriverName].x--;
+        }
+        else if(currChar == '>') {
+            deliverers[currDriverName].y++;
+        }
+        else if(currChar == '<') {
+            deliverers[currDriverName].y--;
+        }
+        
+        goToHouse(deliverers[currDriverName].x,deliverers[currDriverName].y);
+    }
+    
+    return {
+        presToHouse: luckyHouses
+        ,housesVisited: totalHouses
+    };
+}
