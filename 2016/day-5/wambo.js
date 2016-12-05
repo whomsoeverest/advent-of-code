94f681e269c60fa01a1e383cc4c910899eb96619
Nick Jurista <nickjurista@gmail.com>
2016-12-05 17:45:36 +0000

    day 5:

---
+++ 2016/day-5/wambo.js
@@ -0,0 +1,75 @@
+'use strict';
+const md5 = require('./md5');
+
+// Part 1
+function part1() {
+    let input = 'ffykfhsq',
+        password = '',
+        i = 0,
+        hash;
+
+    while (password.length < 8) {
+        hash = md5(input + i);
+        if (hash.substr(0, 5) === '00000') {
+            password += hash.substr(5, 1);
+            console.log('pwd', password, hash);
+        }
+        i++;
+    }
+
+    return password;
+}
+
+console.log(part1());
+
+// Part 2
+function l33t(pwd) {
+    let pass = [];
+
+    for(let i = 0; i < 8; i++) {
+        switch (pwd[i]) {
+            case '/':
+                pass[i] = '-';
+                break;
+            case '-':
+                pass[i] = '\\';
+                break;
+            case '\\':
+                pass[i] = '|';
+                break;
+            case '|':
+                pass[i] = '/';
+                break;
+        }
+    }
+
+    return pass;
+}
+
+function part2() {
+    let input = 'ffykfhsq',
+        password = [],
+        pos = {},
+        text = '',
+        i = 0,
+        hash;
+
+    while (text.length < 8) {
+        hash = md5(input + i);
+        if (hash.substr(0, 5) === '00000') {
+            let position = parseInt(hash.substr(5, 1), 10);
+            if (position >= 0 && position <= 7) {
+                if (!pos[position]) {
+                    text += hash.substr(6, 1);
+                    pos[position] = hash.substr(6, 1);
+                    password[position] = hash.substr(6, 1);
+                }
+            }
+        }
+        i++;
+    }
+
+    return password.join('');
+}
+
+console.log(part2());
